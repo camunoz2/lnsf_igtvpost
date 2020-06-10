a1949ad6d28a125ede6ed1d016c0a4fad4780114
camunoz2 <camunoz2@gmail.com>
2020-06-10 18:43:27 -0400

    added text

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -11,14 +10,12 @@ const IndexPage = () => {
   return (
     <Layout>
       <SEO title="Home" />
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
+      <h1>Liceo Nibaldo Sepulveda - Links IGTV</h1>
+      <p>Hola! aca estan los links para los grupos de whatsapp.</p>
+      <p>Saluditos!.</p>
       <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
         <Image />
       </div>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
     </Layout>
   )
 }
